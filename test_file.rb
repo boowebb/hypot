c149ab6e97419763bc2d6f510782ef91cf65f942
Jeff Webb <jeff@waterfall.com>
2016-09-13 09:52:50 -0700

    add tests for both upto / downto and vice versa as well as the original solution I offered

--- test_file.rb
+++ test_file.rb
@@ -19,16 +19,25 @@ def test_to(upper_bound)
   end
 
   puts "RUNNING TEST 1 (1..#{ true_arr.max })"
-  run_test(true_arr, max_sum, "sum_of_squares_1?")
+  run_test(true_arr, max_sum, "sum_of_squares_1?")     # tests aggressive loop
 
   puts "RUNNING TEST 2 (1..#{ true_arr.max })"
-  run_test(true_arr, max_sum, "sum_of_squares_2?")
+  run_test(true_arr, max_sum, "sum_of_squares_2?")     # tests typical ruby enumeration
 
   puts "RUNNING TEST 3 (1..#{ true_arr.max })"
-  run_test(true_arr, max_sum, "sum_of_squares_3?")
+  run_test(true_arr, max_sum, "sum_of_squares_3?")     # tests integer UPTO (both loops)
 
   puts "RUNNING TEST 4 (1..#{ true_arr.max })"
-  run_test(true_arr, max_sum, "sum_of_squares_4?")
+  run_test(true_arr, max_sum, "sum_of_squares_4?")     # tests integer DOWNTO (both loops)
+
+  puts "RUNNING TEST 5 (1..#{ true_arr.max })"
+  run_test(true_arr, max_sum, "sum_of_squares_5?")     # tests UPTO / DOWNTO
+
+  puts "RUNNING TEST 6 (1..#{ true_arr.max })"
+  run_test(true_arr, max_sum, "sum_of_squares_6?")     # tests DOWNTO / UPTO
+
+  puts "RUNNING TEST 7 (1..#{ true_arr.max })"
+  run_test(true_arr, max_sum, "sum_of_squares_7?")     # original solution
 end
 
 def run_test(true_arr, max_sum, test_method)
@@ -40,9 +49,9 @@ def run_test(true_arr, max_sum, test_method)
 #    puts "#{ test_num } : #{ result }"
     case
     when true_arr.include?(test_num) && !result
-      raise "function should have returned true!"
+      raise "function should have returned true! #{ test_num }"
     when !true_arr.include?(test_num) && result
-      raise "function should have returned false!"
+      raise "function should have returned false! #{ test_num }"
     end
   end
 
@@ -55,10 +64,10 @@ rescue => e
   puts ""
 end
 
-
+# this does not work - this attempts a loop up and fail when you exceed the test num
+# but fails too aggressively
 def sum_of_squares_1?(num)
   idx = 1
-  jdx = 1
 
   while true
     (1..idx).each do |jdx|
@@ -71,8 +80,8 @@ def sum_of_squares_1?(num)
   end
 end
 
+# using "regular" iteration
 def sum_of_squares_2?(num)
-#debugger if num == 173
   max_num = Math.sqrt(num).to_i
 
   (1..max_num).each do |idx|
@@ -85,8 +94,8 @@ def sum_of_squares_2?(num)
   return false
 end
 
+# using INT.upto
 def sum_of_squares_3?(num)
-#debugger if num == 173
   max_num = Math.sqrt(num).to_i
 
   1.upto(max_num) do |idx|
@@ -99,8 +108,8 @@ def sum_of_squares_3?(num)
   return false
 end
 
+# using INT.downto
 def sum_of_squares_4?(num)
-#debugger if num == 173
   max_num = Math.sqrt(num).to_i
 
   max_num.downto(1) do |idx|
@@ -113,5 +122,44 @@ def sum_of_squares_4?(num)
   return false
 end
 
+# using INT.upto / downto
+def sum_of_squares_5?(num)
+  max_num = Math.sqrt(num).to_i
 
-test_to(120)
+  1.upto(max_num) do |idx|
+    idx.downto(1) do |jdx|
+      result = idx**2+jdx**2
+      return true if result == num
+    end
+  end
+
+  return false
+end
+
+# using INT.downto / upto
+def sum_of_squares_6?(num)
+  max_num = Math.sqrt(num).to_i
+
+  max_num.downto(1) do |idx|
+    1.upto(idx) do |jdx|
+      result = idx**2+jdx**2
+      return true if result == num
+    end
+  end
+
+  return false
+end
+
+# original solution!
+def sum_of_squares_7?(num)
+  (1..num).each do |idx|
+    (1..idx).each do |jdx|
+      result = idx**2+jdx**2
+      return true if result == num
+    end
+  end
+
+  return false
+end
+
+test_to(20)
